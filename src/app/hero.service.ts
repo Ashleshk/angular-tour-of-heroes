4c1d2713b7dc2f731723cbc0279cd406a188ee08
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-10 09:15:28 +0530

    Hero Service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-
+import { Observable, of } from 'rxjs';
+//You used RxJS of() to return an observable of mock heroes
+// (Observable<Hero[]>).
+import { MessageService } from './message.service';
+//The HeroService injected into a component is created with another injected service, MessageService.
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
-  getHeroes(): Hero[] {
-    return HEROES;
+  //The component's ngOnInit lifecycle hook calls the HeroService method, not the constructor. 
+  constructor(private messageService:MessageService) {
+    //This is a typical "service-in-service" scenario: you inject 
+    // the MessageService into the HeroService which is injected into 
+    // the HeroesComponent.
+   }
+
+
+  // getHeroes(): Hero[] {  //OLD
+  //   return HEROES;
+  // }
+
+  getHeroes(): Observable<Hero[]> {  //NEW
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 }
 
