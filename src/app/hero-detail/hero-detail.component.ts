2911dc2ec2c068d9aa2e2e79031a6371b5817737
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-11 16:34:43 +0530

    Hero Details Component
    HTML
    JS
    CSS

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -14,18 +14,49 @@ export class HeroDetailComponent implements OnInit {
   hero: Hero;
   constructor(private route: ActivatedRoute,
     private heroService: HeroService,
-    private location: Location) { }
+    private location: Location) {
+      // The ActivatedRoute holds information about the route to this 
+      // instance of the HeroDetailComponent. 
+
+      // This component is interested in the route's parameters 
+      //extracted from the URL. The "id" parameter is the id of
+      // the hero to display.
+
+      // The location is an Angular service for interacting with the
+      //  browser. You'll use it later to navigate back to the view 
+      //  that navigated here.
+     }
 
   ngOnInit(): void {
     this.getHero();
   }
+
+  //Extract the id route parameter from URL
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
+    // The route.snapshot is a static image of the route information shortly after 
+    // the component was created.
+
+    // The paramMap is a dictionary of route parameter values extracted from the URL.
+    //  The "id" key returns the id of the hero to fetch.
+
+    // Route parameters are always strings. The JavaScript (+) operator converts the
+    //  string to a number, which is what a hero id should be.
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
-    this.location.back();
+    this.location.back(); 
+    // navigates backward one step in the browser's history stack
+    // using the Location service 
+  }
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+      // In the HeroDetail component class, add the following save()
+      //  method, which persists hero name changes using the hero
+      //   service updateHero() method and then navigates back to
+      //    the previous view.
   }
 }
